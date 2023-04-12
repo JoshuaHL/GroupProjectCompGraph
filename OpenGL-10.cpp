455916f93055b7ef1e2137e890c36c76984aec42
Joshua Lashley <joshua.h.lashley@gmail.com>
2023-04-12 16:18:59 -0400

    update

--- OpenGL-10.cpp
+++ OpenGL-10.cpp
@@ -176,11 +176,11 @@ void mouse_callback(GLFWwindow* window, int button, int action, int mods) {
         ballxdif = (ballPositions[15][0] + 198) - (396 * (mouseX / 1280));
         ballydif = (ballPositions[15][1] + 105) - (210 * ((720 - mouseY) / 720));
         totaldiff = abs(ballxdif) + abs(ballydif);
-        ballVelocities[15][0] = - (ballxdif / totaldiff * 2);
-        ballVelocities[15][1] = - (ballydif / totaldiff * 2);
-       //     cueBallPosX = getCueBallX(); // Function to get the current cue ball position
-       //     cueBallPosY = getCueBallY();
-       // }
+        ballVelocities[15][0] = -(ballxdif / totaldiff * 2);
+        ballVelocities[15][1] = -(ballydif / totaldiff * 2);
+        //     cueBallPosX = getCueBallX(); // Function to get the current cue ball position
+        //     cueBallPosY = getCueBallY();
+        // }
     }
     else if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_RELEASE) {
         dragging = false;
@@ -202,7 +202,7 @@ int main()
     init_Resources();
 
     //-------- PoolBall initial values -------------------
-    
+
     ballPositions[0][0] = 132.0;
     ballPositions[0][1] = 14.0;
     ballPositions[1][0] = 93.0;
@@ -236,10 +236,10 @@ int main()
     ballPositions[15][0] = 10.0;
     ballPositions[15][1] = 10.0;
 
-    
+
 
     ballVelocities[15][0] = 0.0;
-    
+
 
     //------------------------------------------------
 
@@ -252,191 +252,191 @@ int main()
 // ==============================================
 
 // 1. Setup and compile the shader
-Shader poolBallShader("poolBallVertex.glsl", "poolBallFragment.glsl");
+    Shader poolBallShader("poolBallVertex.glsl", "poolBallFragment.glsl");
 
 
-// 2. Load the earth. mars and venus objects
-Model Ball1((GLchar*)"1Ball.obj");
-Model Ball2((GLchar*)"2Ball.obj");
-Model Ball3((GLchar*)"3Ball.obj");
-Model Ball4((GLchar*)"4Ball.obj");
-Model Ball5((GLchar*)"5Ball.obj");
-Model Ball6((GLchar*)"6Ball.obj");
-Model Ball7((GLchar*)"7Ball.obj");
-Model Ball8((GLchar*)"8Ball.obj");
-Model Ball9((GLchar*)"9Ball.obj");
-Model Ball10((GLchar*)"10Ball.obj");
-Model Ball11((GLchar*)"11Ball.obj");
-Model Ball12((GLchar*)"12Ball.obj");
-Model Ball13((GLchar*)"13Ball.obj");
-Model Ball14((GLchar*)"14Ball.obj");
-Model Ball15((GLchar*)"15Ball.obj");
-Model CueBall((GLchar*)"CueBall.obj");
-
-
-
-
-// 3. Set the projection matrix for the camera
-glm::mat4 projection = glm::perspective(45.0f, (GLfloat)sWidth / (GLfloat)sHeight,
-    1.0f, 10000.0f);
-
-
-
-// 4. Create the projection matrices by Activating the shader objects
-
-poolBallShader.Use();
-glUniformMatrix4fv(glGetUniformLocation(poolBallShader.Program, "projection"),
-    1, GL_FALSE, glm::value_ptr(projection));
+    // 2. Load the earth. mars and venus objects
+    Model Ball1((GLchar*)"1Ball.obj");
+    Model Ball2((GLchar*)"2Ball.obj");
+    Model Ball3((GLchar*)"3Ball.obj");
+    Model Ball4((GLchar*)"4Ball.obj");
+    Model Ball5((GLchar*)"5Ball.obj");
+    Model Ball6((GLchar*)"6Ball.obj");
+    Model Ball7((GLchar*)"7Ball.obj");
+    Model Ball8((GLchar*)"8Ball.obj");
+    Model Ball9((GLchar*)"9Ball.obj");
+    Model Ball10((GLchar*)"10Ball.obj");
+    Model Ball11((GLchar*)"11Ball.obj");
+    Model Ball12((GLchar*)"12Ball.obj");
+    Model Ball13((GLchar*)"13Ball.obj");
+    Model Ball14((GLchar*)"14Ball.obj");
+    Model Ball15((GLchar*)"15Ball.obj");
+    Model CueBall((GLchar*)"CueBall.obj");
 
 
 
 
-/*--////////////////Section below - Done by Zachary Farrell/////////////--*/
-/////////////////////////////////////////////////////////////////////////////
-
-
-Model poolStick((GLchar*)"10522_Pool_Cue_v1_L3.obj");
-
-
-/*--////////////////Section above - Done by Zachary Farrell/////////////--*/
-/////////////////////////////////////////////////////////////////////////////
+    // 3. Set the projection matrix for the camera
+    glm::mat4 projection = glm::perspective(45.0f, (GLfloat)sWidth / (GLfloat)sHeight,
+        1.0f, 10000.0f);
 
 
 
-
- // ==================================================================
- // ====== Set up the changes we want while the window is open =======
- // ==================================================================
-
-while (!glfwWindowShouldClose(window))
-{
-
-    // Clear buffers
-    glClearColor(0.0f, 0.345f, 0.141f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-
-    // Add transformation matrices ... by repeatedly modifying the model matrix
-
-    // 1. The View matrix for each planet first...
+    // 4. Create the projection matrices by Activating the shader objects
 
     poolBallShader.Use();
-    glUniformMatrix4fv(glGetUniformLocation(poolBallShader.Program, "view"), 1,
-        GL_FALSE, glm::value_ptr(camera.GetViewMatrix()));
-
-
-    // 2. Create the model matrix for each pool ball
-
-    poolBallShader.Use();
-    glm::mat4 Ball1Model = glm::mat4(1);
-    glm::mat4 Ball2Model = glm::mat4(1);
-    glm::mat4 Ball3Model = glm::mat4(1);
-    glm::mat4 Ball4Model = glm::mat4(1);
-    glm::mat4 Ball5Model = glm::mat4(1);
-    glm::mat4 Ball6Model = glm::mat4(1);
-    glm::mat4 Ball7Model = glm::mat4(1);
-    glm::mat4 Ball8Model = glm::mat4(1);
-    glm::mat4 Ball9Model = glm::mat4(1);
-    glm::mat4 Ball10Model = glm::mat4(1);
-    glm::mat4 Ball11Model = glm::mat4(1);
-    glm::mat4 Ball12Model = glm::mat4(1);
-    glm::mat4 Ball13Model = glm::mat4(1);
-    glm::mat4 Ball14Model = glm::mat4(1);
-    glm::mat4 Ball15Model = glm::mat4(1);
-    glm::mat4 CueBallModel = glm::mat4(1);
-
-
-    for (int i = 0; i < 16; i++) {
-        if (ballPositions[i][0] > 190){
-            ballVelocities[i][0] *= -1;
-            ballPositions[i][0] = 190;
-        }
-
-        if (ballPositions[i][0] < -190) {
-            ballVelocities[i][0] *= -1;
-            ballPositions[i][0] = -190;
-        }
-
-        if (ballPositions[i][1] > 105) {
-            ballVelocities[i][1] *= -1;
-            ballPositions[i][1] = 105;
-        }
-
-        if (ballPositions[i][1] < -105) {
-            ballVelocities[i][1] *= -1;
-            ballPositions[i][1] = -105;
-        }
-    }
-
-    for (int i = 0; i < 16; i++) {
-        for (int j = i + 1; j < 16; j++) {
-            float dx = ballPositions[j][0] - ballPositions[i][0];
-            float dy = ballPositions[j][1] - ballPositions[i][1];
-            //Distance between the center of each ball
-            float distance = sqrt(dx * dx + dy * dy);
-            if (distance < 2 * BALL_RADIUS) {
-                float v1[2];
-                float v2[2];
-                float u1[2];
-                float u2[2];
-                float nx = dx / distance;
-                float ny = dy / distance;
-                float overlap = (BALL_RADIUS * 2 - distance);
-
-            
-                //Calculate the angle of rotation
-                float theta = -atan2(ballPositions[i][1] - ballPositions[j][1], ballPositions[i][0] - ballPositions[j][0]);
-                //rotate angle
-                u1[0] = ballVelocities[i][0] * cos(theta) - ballVelocities[i][1] * sin(theta);
-                u1[1] = ballVelocities[i][0] * sin(theta) + ballVelocities[i][1] * cos(theta);
-                u2[0] = ballVelocities[j][0] * cos(theta) - ballVelocities[j][1] * sin(theta);
-                u2[1] = ballVelocities[j][0] * sin(theta) + ballVelocities[j][1] * cos(theta);
-                v1[0] = u2[0] * cos(-theta) - u1[1] * sin(-theta);
-                v1[1] = u2[0] * sin(-theta) + u1[1] * cos(-theta);
-                v2[0] = u1[0] * cos(-theta) - u2[1] * sin(-theta);
-                v2[1] = u1[0] * sin(-theta) + u2[1] * cos(-theta);
-
-                float overlapDirectionX = dx / distance;
-                float overlapDirectionY = dy / distance;
-                float totalOverlap = overlapDirectionX + overlapDirectionY;
-
-                ballVelocities[i][0] = ballVelocities[i][0] * 0.2 + v1[0];
-                ballVelocities[i][1] = ballVelocities[i][1] * 0.2 + v1[1];
-                ballVelocities[j][0] = ballVelocities[j][0] * 0.2 + v2[0];
-                ballVelocities[j][1] = ballVelocities[j][1] * 0.2 + v2[1];
+    glUniformMatrix4fv(glGetUniformLocation(poolBallShader.Program, "projection"),
+        1, GL_FALSE, glm::value_ptr(projection));
 
 
 
-                // Move the balls away from each other along the direction of overlap
-                
-                ballPositions[i][0] -= overlapDirectionX * overlap / 2.0f;
-                ballPositions[i][1] -= overlapDirectionY * overlap / 2.0f;
-                ballPositions[j][0] += overlapDirectionX * overlap / 2.0f;
-                ballPositions[j][1] += overlapDirectionY * overlap / 2.0f;
-        
+
+    /*--////////////////Section below - Done by Zachary Farrell/////////////--*/
+    /////////////////////////////////////////////////////////////////////////////
+
+
+    Model poolStick((GLchar*)"10522_Pool_Cue_v1_L3.obj");
+
+
+    /*--////////////////Section above - Done by Zachary Farrell/////////////--*/
+    /////////////////////////////////////////////////////////////////////////////
+
+
+
+
+     // ==================================================================
+     // ====== Set up the changes we want while the window is open =======
+     // ==================================================================
+
+    while (!glfwWindowShouldClose(window))
+    {
+
+        // Clear buffers
+        glClearColor(0.0f, 0.345f, 0.141f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+
+        // Add transformation matrices ... by repeatedly modifying the model matrix
+
+        // 1. The View matrix for each planet first...
+
+        poolBallShader.Use();
+        glUniformMatrix4fv(glGetUniformLocation(poolBallShader.Program, "view"), 1,
+            GL_FALSE, glm::value_ptr(camera.GetViewMatrix()));
+
+
+        // 2. Create the model matrix for each pool ball
+
+        poolBallShader.Use();
+        glm::mat4 Ball1Model = glm::mat4(1);
+        glm::mat4 Ball2Model = glm::mat4(1);
+        glm::mat4 Ball3Model = glm::mat4(1);
+        glm::mat4 Ball4Model = glm::mat4(1);
+        glm::mat4 Ball5Model = glm::mat4(1);
+        glm::mat4 Ball6Model = glm::mat4(1);
+        glm::mat4 Ball7Model = glm::mat4(1);
+        glm::mat4 Ball8Model = glm::mat4(1);
+        glm::mat4 Ball9Model = glm::mat4(1);
+        glm::mat4 Ball10Model = glm::mat4(1);
+        glm::mat4 Ball11Model = glm::mat4(1);
+        glm::mat4 Ball12Model = glm::mat4(1);
+        glm::mat4 Ball13Model = glm::mat4(1);
+        glm::mat4 Ball14Model = glm::mat4(1);
+        glm::mat4 Ball15Model = glm::mat4(1);
+        glm::mat4 CueBallModel = glm::mat4(1);
+
+
+        for (int i = 0; i < 16; i++) {
+            if (ballPositions[i][0] > 190) {
+                ballVelocities[i][0] *= -1;
+                ballPositions[i][0] = 190;
+            }
+
+            if (ballPositions[i][0] < -190) {
+                ballVelocities[i][0] *= -1;
+                ballPositions[i][0] = -190;
+            }
+
+            if (ballPositions[i][1] > 105) {
+                ballVelocities[i][1] *= -1;
+                ballPositions[i][1] = 105;
+            }
+
+            if (ballPositions[i][1] < -105) {
+                ballVelocities[i][1] *= -1;
+                ballPositions[i][1] = -105;
             }
         }
-    }
 
-    //Opdate all ball positions based on their velocities with a slight friction
-    for (int i = 0; i < 16; i++) {
-        ballPositions[i][0] += ballVelocities[i][0];
-        ballPositions[i][1] += ballVelocities[i][1];
-        if (ballVelocities[i][0] > 0) {
-            ballVelocities[i][0] *= 0.996;
-        } 
-        if (ballVelocities[i][0] < 0) {
-            ballVelocities[i][0] *= 0.996;
-        }
-        if (ballVelocities[i][1] > 0) {
-            ballVelocities[i][1] *= 0.996;
-        }
-        if (ballVelocities[i][1] < 0) {
-            ballVelocities[i][1] *= 0.996;
-        }
-    }
+        for (int i = 0; i < 16; i++) {
+            for (int j = i + 1; j < 16; j++) {
+                float dx = ballPositions[j][0] - ballPositions[i][0];
+                float dy = ballPositions[j][1] - ballPositions[i][1];
+                //Distance between the center of each ball
+                float distance = sqrt(dx * dx + dy * dy);
+                if (distance < 2 * BALL_RADIUS) {
+                    float v1[2];
+                    float v2[2];
+                    float u1[2];
+                    float u2[2];
+                    float nx = dx / distance;
+                    float ny = dy / distance;
+                    float overlap = (BALL_RADIUS * 2 - distance);
 
-    
+
+                    //Calculate the angle of rotation
+                    float theta = -atan2(ballPositions[i][1] - ballPositions[j][1], ballPositions[i][0] - ballPositions[j][0]);
+                    //rotate angle
+                    u1[0] = ballVelocities[i][0] * cos(theta) - ballVelocities[i][1] * sin(theta);
+                    u1[1] = ballVelocities[i][0] * sin(theta) + ballVelocities[i][1] * cos(theta);
+                    u2[0] = ballVelocities[j][0] * cos(theta) - ballVelocities[j][1] * sin(theta);
+                    u2[1] = ballVelocities[j][0] * sin(theta) + ballVelocities[j][1] * cos(theta);
+                    v1[0] = u2[0] * cos(-theta) - u1[1] * sin(-theta);
+                    v1[1] = u2[0] * sin(-theta) + u1[1] * cos(-theta);
+                    v2[0] = u1[0] * cos(-theta) - u2[1] * sin(-theta);
+                    v2[1] = u1[0] * sin(-theta) + u2[1] * cos(-theta);
+
+                    float overlapDirectionX = dx / distance;
+                    float overlapDirectionY = dy / distance;
+                    float totalOverlap = overlapDirectionX + overlapDirectionY;
+
+                    ballVelocities[i][0] = ballVelocities[i][0] * 0.2 + v1[0];
+                    ballVelocities[i][1] = ballVelocities[i][1] * 0.2 + v1[1];
+                    ballVelocities[j][0] = ballVelocities[j][0] * 0.2 + v2[0];
+                    ballVelocities[j][1] = ballVelocities[j][1] * 0.2 + v2[1];
+
+
+
+                    // Move the balls away from each other along the direction of overlap
+
+                    ballPositions[i][0] -= overlapDirectionX * overlap / 2.0f;
+                    ballPositions[i][1] -= overlapDirectionY * overlap / 2.0f;
+                    ballPositions[j][0] += overlapDirectionX * overlap / 2.0f;
+                    ballPositions[j][1] += overlapDirectionY * overlap / 2.0f;
+
+                }
+            }
+        }
+
+        //Opdate all ball positions based on their velocities with a slight friction
+        for (int i = 0; i < 16; i++) {
+            ballPositions[i][0] += ballVelocities[i][0];
+            ballPositions[i][1] += ballVelocities[i][1];
+            if (ballVelocities[i][0] > 0) {
+                ballVelocities[i][0] *= 0.996;
+            }
+            if (ballVelocities[i][0] < 0) {
+                ballVelocities[i][0] *= 0.996;
+            }
+            if (ballVelocities[i][1] > 0) {
+                ballVelocities[i][1] *= 0.996;
+            }
+            if (ballVelocities[i][1] < 0) {
+                ballVelocities[i][1] *= 0.996;
+            }
+        }
+
+
 
 
 
@@ -499,7 +499,7 @@ while (!glfwWindowShouldClose(window))
         Ball15Model = glm::rotate(Ball15Model, -45.0f, glm::vec3(1.0f, 0.0f, 0.0f));
         CueBallModel = glm::rotate(CueBallModel, -45.0f, glm::vec3(1.0f, 0.0f, 0.0f));
 
-        GLfloat BallAngles[16] = {-45.0f};
+        GLfloat BallAngles[16] = { -45.0f };
 
         glm::mat4* BallModels[16] = { &Ball1Model, &Ball2Model, &Ball3Model, &Ball4Model, &Ball5Model, &Ball6Model, &Ball7Model, &Ball8Model, &Ball9Model, &Ball10Model, &Ball11Model, &Ball12Model, &Ball13Model, &Ball14Model, &Ball15Model, &CueBallModel };
 
@@ -521,10 +521,10 @@ while (!glfwWindowShouldClose(window))
                 *BallModels[i] = glm::rotate(*BallModels[i], angle[i], axis);
             }
         }
-    
 
-        
-        
+
+
+
 
         glUniformMatrix4fv(glGetUniformLocation(poolBallShader.Program, "model"), 1,
             GL_FALSE, glm::value_ptr(Ball1Model));
@@ -540,7 +540,7 @@ while (!glfwWindowShouldClose(window))
             GL_FALSE, glm::value_ptr(Ball3Model));
 
         Ball3.Draw(poolBallShader);
-        
+
         glUniformMatrix4fv(glGetUniformLocation(poolBallShader.Program, "model"), 1,
             GL_FALSE, glm::value_ptr(Ball4Model));
 
